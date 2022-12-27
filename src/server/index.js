ad5b5e066a3fde5068cc2b832687bb72bdd42a25
Khoi Vu <khoivukha0509@gmail.com>
2022-12-27 14:11:50 +0700

    Add Geonames, Weatherbit and Pixabay API to web app

--- src/server/index.js
+++ src/server/index.js
@@ -2,8 +2,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 const fetch = require('node-fetch');
 
+const geoNamesUserName = process.env.GEONAMES_USERNAME;
+const weatherBitApiKey = process.env.WEATHERBIT_API_KEY;
+const pixaBayApiKey = process.env.PIXABAY_API_KEY;
+
 // Setup empty JS object to act as endpoint for all routes
 let projectData = {};
+
+// An array contains save trips.
+let savedTrips = [];
 
 var path = require('path');
 // Require Express to run server and routes
@@ -31,22 +38,45 @@ app.use(express.static('dist'));
 
 console.log(__dirname);
 
-app.get('/', function(req, res) {
-    res.sendFile('dist/index.html');
-})
-
-const port = 8080;
-// Setup Server
-// designates what port the app will listen to for incoming requests
-app.listen(port, function() {
-    console.log(`[Server] listening on port: ${port}`);
-})
-
+// GET Routes
 app.get('/test', function(req, res) {
     res.send(mockAPIResponse);
 });
 
-// POST route
-app.post('/add', addData);
-async function addData(req, res) {
+app.get('/', function(req, res) {
+    res.sendFile('dist/index.html');
+})
+
+// POST routes
+app.post('/geonames-location', geoNamesLocation);
+async function geoNamesLocation(req, res) {
+    const response = await axios.get(
+        `${req.body.baseURL}&username=${geoNamesUserName}`
+    );
+    res.send(response.data);
 };
+
+// Setup Server
+// designates what port the app will listen to for incoming requests
+const port = 8080;
+app.listen(port, function() {
+    console.log(`[Server] listening on port: ${port}`);
+
+    if (geoNamesUserName) {
+        console.log("GEONAMES_USERNAME: ", geoNamesUserName);
+    } else {
+        console.error("Error config for GEONAMES_USERNAME");
+    }
+
+    if (weatherBitApiKey) {
+        console.log("WEATHERBIT_API_KEY: ", weatherBitApiKey);
+    } else {
+        console.error("Error config for WEATHERBIT_API_KEY");
+    }
+
+    if (pixaBayApiKey) {
+        console.log("PIXABAY_API_KEY: ", pixaBayApiKey);
+    } else {
+        console.error("Error config for PIXABAY_API_KEY");
+    }
+})
